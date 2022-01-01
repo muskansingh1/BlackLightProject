f2c57b260301dc3340be3dc45129eb1b0640bd6e
Muskan Singh <muskans1@birlasoft.com>
2022-01-01 12:45:38 +0530

    init

--- scripts/index.js
+++ scripts/index.js
@@ -23,8 +23,16 @@ function SortTables(table,data){
       var selected = $('#MaxColumns').val();
       var sortedObjs = _.sortBy( data, selected );
       console.log(sortedObjs)
-      // showTable(sortedObjs)
+      showTableAfterSort(sortedObjs)
     })
+}
+
+function showTableAfterSort(data){
+  for(let i=0;i<data.length;i++){
+    var x=document.getElementsByClassName('table')[0].deleteRow(1);
+  } 
+
+  showTable(data);
 }
 
 function getPagination(table) {
@@ -135,4 +143,62 @@ function getPagination(table) {
 
 		}
 	}
+}
+
+$('table.table-sortable th').on('click', function(e) {
+  sortTableByColumn(this)
+})
+
+function sortTableByColumn(tableHeader) {
+  // extract all the relevant details
+  let table = tableHeader.closest('table')
+  let index = tableHeader.cellIndex
+  let sortType = tableHeader.dataset.sortType
+  let sortDirection = tableHeader.dataset.sortDir || 'asc' // default sort to ascending
+
+  // sort the table rows
+  let items = Array.prototype.slice.call(table.rows);
+  let sortFunction = getSortFunction(sortType, index, sortDirection)
+  let sorted = items.sort(sortFunction)
+
+  // remove and re-add rows to table
+  
+  for (let row of sorted) {
+    let parent = row.parentNode
+    if(row.rowIndex != 0){
+      let detatchedItem = parent.removeChild(row)
+      parent.appendChild(row)
+    }
+  }
+
+  // reset heading values and styles
+  for (let header of tableHeader.parentNode.children) {
+    header.classList.remove('currently-sorted')
+    delete header.dataset.sortDir
+  }
+
+  // update this headers's values and styles
+  tableHeader.dataset.sortDir = sortDirection == 'asc' ? 'desc' : 'asc'
+  tableHeader.classList.add('currently-sorted')
+}
+
+function getSortFunction(sortType, index, sortDirection) {
+  let dir = sortDirection == 'asc' ? -1 : 1
+  switch (sortType) {
+    case 'text': return stringRowComparer(index, dir);
+    case 'numeric': return numericRowComparer(index, dir);
+    default: return stringRowComparer(index, dir);
+  }
+}
+
+// asc = alphanumeric order - eg 0->9->a->z
+// desc = reverse alphanumeric order - eg z->a->9->0
+function stringRowComparer(index, direction) {
+  return (a, b) => -1 * direction * a.children[index].textContent.localeCompare(b.children[index].textContent)
+}
+
+// asc = higest to lowest - eg 999->0
+// desc = lowest to highest - eg 0->999
+function numericRowComparer(index, direction) {
+  return (a, b) => direction * (Number(a.children[index].textContent) - Number(b.children[index].textContent))
 }
